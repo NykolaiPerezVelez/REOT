ea59c67b30f06d413a499949f14a64b1c0026baa
NykolaiPerez <nperez_dcao_smn@outlook.com>
2016-11-04 10:25:13 -0300

    PostCharlaConLaMujer
    Principio de cambios en lo colore, y mas parametros de entrada

---
+++ REOTv1.01.005/REOT/app/src/main/java/com/example/nico_/reot/Envio.java
@@ -0,0 +1,105 @@
+package com.example.nico_.reot;
+
+
+
+    import android.app.ProgressDialog;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.util.Log;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.TextView;
+    import android.widget.Toast;
+
+    import butterknife.ButterKnife;
+    import butterknife.InjectView;
+
+
+    public class Envio extends AppCompatActivity {
+        private static final String TAG = "Ingreso como censista";
+
+
+        @InjectView(R.id.btn_arboles)
+        Button _arbolesButton;
+
+
+        @InjectView(R.id.gracias)
+        TextView _gracias;
+
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_envio);
+            ButterKnife.inject(this);
+
+            _arbolesButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    signup();
+                }
+            });
+
+            ;
+        }
+
+
+        public void signup() {
+            Log.d(TAG, "Signup");
+
+            if (!validate()) {
+                onSignupFailed();
+                return;
+            }
+
+            _arbolesButton.setEnabled(false);
+
+            final ProgressDialog progressDialog = new ProgressDialog(Envio.this,
+                    R.style.AppTheme_Dark_Dialog);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage("Generando planilla...");
+            progressDialog.show();
+
+
+            // TODO: Implement your own signup logic here.
+
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            // On complete call either onSignupSuccess or onSignupFailed
+                            // depending on success
+                            onSignupSuccess();
+                            // onSignupFailed();
+                            progressDialog.dismiss();
+                        }
+                    }, 3000);
+        }
+
+
+        public void onSignupSuccess() {
+            _arbolesButton.setEnabled(true);
+            setResult(RESULT_OK, null);
+
+            Intent datosArboles1 = new Intent(Envio.this, LoginActivity.class);
+            startActivity(datosArboles1);
+            //
+            //
+            //
+            // finish();
+        }
+
+        public void onSignupFailed() {
+            Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+            _arbolesButton.setEnabled(true);
+        }
+
+        public boolean validate() {
+            boolean valid = true;
+
+
+            return valid;
+        }
+    }
+
